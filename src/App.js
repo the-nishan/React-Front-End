44f088ecc013d554c6e9a0b8de089bcc3a878c43
Didaur Rahman <>
2025-10-19 19:01:28 +0600

    moc web app (partial)

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Helmet } from "react-helmet"; // for <head> tags like <title>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="https://google.com">
-          {" "}
-          <img src="https://tinyurl.com/5ckz33t5" />
-        </a>
-        <h2 className="text-black -600 font-bold">Save Earth today for tomorrow</h2>
+    <div className="min-h-screen bg-gray-100 text-gray-800 flex flex-col items-center">
+      <Helmet>
+        <title>Moc webApp</title>
+        <meta name="description" content="its a moc page"></meta>
+      </Helmet>
+      {/* starts here */}
+      <h2>Log In</h2>
+      <h2>Use your email and password to continue</h2>
+      <h2>Email</h2>
 
-        <p>
-          <h3>
-            Changes in the <em>winter</em> environment:
-          </h3>
-          <ul>
-            <li>Ice and snow</li>
-            <li>Global temperature and oceans</li>
-            <li>Weather patterns</li>
-          </ul>
-        </p>
+      <input
+        type="box"
+        placeholder="Enter your email"
+        class="border border-red-700 rounded-md p-2 w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
 
-        <h5>Are you aware of this?</h5>
+      <h2>Password</h2>
+      <input
+        type="box"
+        placeholder="Enter your Password"
+        class="border border-green-700 rounded-md p-2 w-64 focus:outline-none focus:ring-2 focus:ring-red-500"
+      />
 
-        <a
-          className="App-link"
-          href="https://tinyurl.com/4aj9v5dm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Search here to lern more!
-        </a>
-      </header>
+      {/* radio button */}
+      <div class="p-4 space-y-2">
+        <label className="inline-flex items left-2  space-x-2">
+          <input
+            type="radio"
+            name="check"
+            value="Remember Me2"
+            className="form-radio text bg-red-500"
+          />
+          <span>Remember Me</span>
+        </label>
+      </div>
+      <button class="  bg-blue-500 text-white font-semibold justify-start px-4 py-2 rounded hover:bg-green-600 active:bg-yellow-700 transition">
+      Sign in
+      </button>
+      <div className="w-full flex justify-start p-4 bg-gray-100">
+      <button class="  bg-pink-500 text-white font-semibold justify-start px-4 py-2 rounded hover:bg-green-600 active:bg-yellow-700 transition">
+      Continue with google
+      </button>
+       <button class="  bg-violet-600 text-white font-semibold justify-end px-4 py-2 rounded hover:bg-green-600 active:bg-yellow-700 transition">
+             Continue with GitHub
+      </button>
+      </div>
+<div className="w full  flex  justify-between  p-4"> 
+<p className="text-red-600 justify-between font-weight-700 space-x-6">Dont have an account?</p>
+<p className="text-red-600  font-weight-700" >Sign up!</p>
+
+
+  </div>
+      {/*  ends here */}
     </div>
   );
 }
