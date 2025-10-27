c72eb399430e53a6bff472278e0fcfcf528b7a0c
Didaur Rahman <>
2025-10-27 15:07:14 +0600

    react counter app function separated

--- src/useState/useStatePractice.js
+++ src/useState/useStatePractice.js
@@ -1,55 +1,116 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; 
 
-function useStatePractice() {
-  const [count, newFunc] = useState(0);
+ 
 
-  return (
-    <div className="h h-screen bg bg-gray-100 justify-center items-center m-4">
-      <div className="flex flex-row justify-center items-center">
-      <div className="flex items-center justify-center mt-4 mb-10">
-        <h2 className="text-amber-950 font-bold text-6xl">Counter App</h2>
-      </div>
-      </div>
+function useStatePractice() { 
 
-      {/*  */}
-      <div className="flex justify-center items-center">
-<h1 className="font-extrabold text-8xl mb-5">
-  {count}
-</h1>
-</div>
-      {/*  */}
+const [count, setCount] = useState(0); 
 
+ let funcIncrease = () => {
+  setCount(count+1)
+ };
 
-<div className="flex justify-between mx-8">
-      <div className="flex rounded-full bg-green-500 w-20 h-20 justify-center items-center">
-<button onClick={()=> newFunc (count+1)}>
- <p className="font-bold text-5xl"> + </p>
-</button>
-</div>
+ let funcDecrease = () => {
+  setCount(count-1)
+ };
 
-<div className="flex">
-      <div className="flex rounded-lg bg-indigo-200 w-20 h-8 justify-center items-center">
-<button onClick={()=> newFunc (count*0)}>
- <p className="font-bold text-xl"> RESET </p>
-</button>
-</div>
-</div>
+ let RESET = () => {
+  setCount(0)
+ };
 
 
-<div className="flex">
-<div className="flex rounded-full bg-red-500 w-20 h-20 justify-center items-center">
-<button onClick={()=> newFunc(count-1)}>
- <p className="text-5xl font-bold"> - </p>
-</button>
-</div>
-</div>
+return ( 
 
-</div>
+<div className="rounded-md h-screen bg bg-gray-100 justify-center items-center m-4"> 
 
+<div className="flex flex-row justify-center items-center"> 
 
-    </div>
+<div className="flex items-center justify-center mt-4 mb-10"> 
 
-    
-  );
-}
-export default useStatePractice;
+<h2 className="text-amber-950 font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Counter App</h2> 
+
+</div> 
+</div> 
+
+ 
+
+{/* */} 
+
+<div className="flex justify-center items-center"> 
+
+<h1 className="font-extrabold text-5xl sm:text-6xl md:text-7xl lg:text-8xl mb-14"> 
+
+{count} 
+
+</h1> 
+
+</div> 
+
+{/* */} 
+
+ 
+
+ 
+
+<div className="flex justify-between mx-8"> 
+
+<div className="flex rounded-full bg-green-500 text-black w-20 h-20 justify-center items-center hover:bg-violet-500 hover:text-white hover:animate-pulse"> 
+
+<button onClick={funcIncrease}> 
+<p className="font-bold text-5xl"> + </p> 
+</button> 
+</div> 
+
+ 
+
+<div className="flex"> 
+
+<div className="flex rounded-2xl bg-indigo-200 w-40 h-13 justify-center items-center hover:bg-orange-200"> 
+
+<button onClick={RESET}> 
+
+<p className="font-bold text-xl"> RESET </p> 
+
+</button> 
+
+</div> 
+
+</div> 
+
+ 
+
+ 
+
+<div className="flex"> 
+
+<div className="flex rounded-full bg-red-500 w-20 h-20 justify-center items-center hover:bg-blue-500 hover:text-white hover:animate-pulse"> 
+
+<button onClick={funcDecrease}> 
+
+<p className="text-5xl font-bold"> - </p> 
+
+</button> 
+
+</div> 
+
+</div> 
+
+ 
+
+</div> 
+
+ 
+
+ 
+
+</div> 
+
+ 
+
+); 
+
+} 
+
+export default useStatePractice; 
+
+ 
