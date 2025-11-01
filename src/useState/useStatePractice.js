cd7d52a444e5af3a366d00c9928b22a0af202725
Tunvir Rahman <tusher@nsales.dk>
2025-11-01 18:00:59 +0600

    Hook Logic updated

--- src/useState/useStatePractice.js
+++ src/useState/useStatePractice.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from "react";
 
+const modeOptions = {
+  INCREMENT:"increment",
+  DECREMENT: "decrement"
+}
+
 function useStatePractice() {
   const [count, setCount] = useState(0);
+  const  [mode,setMode] = useState(modeOptions.INCREMENT) // Default Mode Increment Considered!!
 
-  useEffect(()=>{
-    let newCount = count;
-    if (newCount % 5 === 0) {
-      setCount(count+1);
-}},[count]);
+  useEffect(() => {
+    if (count % 5 === 0) {
+      setCount(mode === modeOptions.INCREMENT ? count+1 : count-1);
+    }
+  }, [count,mode]);
 
   let funcIncrease = () => {
-    setCount(count+1);
+    setCount(count + 1);
+    setMode(modeOptions.INCREMENT)
   };
 
   let funcDecrease = () => {
-      setCount(count-1);
-    };
+    setCount(count - 1);
+    setMode(modeOptions.DECREMENT)
+  };
 
   let RESET = () => {
     setCount(0);
+    setMode(modeOptions.INCREMENT)
   };
 
   return (
